f72ca3bdba0ef5d6d0ad253e5c2a4c0228feb4d6
betingrich4 <mariseltru@gmail.com>
2025-04-29 23:43:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Middleware
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/code', require('./pair'));
@@ -14,14 +12,6 @@ app.get('/pair', (req, res) => res.sendFile(path.join(__dirname, 'pair.html')));
 app.get('/success', (req, res) => res.sendFile(path.join(__dirname, 'views/success.html')));
 app.get('/', (req, res) => res.redirect('/pair'));
 
-// Error handling
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-module.exports = app;
